7621f25fbb340ab3155e1cf2a9c55378c2f0a713
JPsquared <jpjpost1@gmail.com>
2021-04-27 22:19:59 -0400

    Work on action_planning.py

--- action_planning.py
+++ action_planning.py
@@ -5,8 +5,16 @@ from dijkstras_algorithm import Graph
 
 if __name__ == "__main__":
     print("Action Planning Project")
+    # use map of world to get bot position
+
+    # get graph of world from dotfile
+
     # create graph
     g = Graph(10)  # call with number of nodes in graph
     # call add_edge()
+    # do in a loop
+
     # call dijkstra's
+    g.dijkstra(0)  # call with start node
     # call show_path(x, y)
+    g.show_path(0, 9)  # call with start and end node labels
