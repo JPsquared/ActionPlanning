2ba4e06e16aca8958736169d32fbc636105e7b81
JPsquared <jpjpost1@gmail.com>
2021-04-27 23:15:01 -0400

    Work on action_planning.py

--- action_planning.py
+++ action_planning.py
@@ -9,12 +9,20 @@ if __name__ == "__main__":
 
     # get graph of world from dotfile
 
+    # get balloon starting positions
+    with open("test_warehouse_start.json") as json_file:
+        start_pos = json.load(json_file)
+        print start_pos
+    # get balloon ending positions
+    with open("test_warehouse_end.json") as json_file:
+        end_pos = json.load(json_file)
+        print end_pos
     # create graph
-    g = Graph(10)  # call with number of nodes in graph
+    # g = Graph(10)  # call with number of nodes in graph
     # call add_edge()
     # do in a loop
 
     # call dijkstra's
-    g.dijkstra(0)  # call with start node
+    # g.dijkstra(0)  # call with start node
     # call show_path(x, y)
-    g.show_path(0, 9)  # call with start and end node labels
+    # g.show_path(0, 9)  # call with start and end node labels
