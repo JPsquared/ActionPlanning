3c0b8f6a1d2c3e687f57fd8a75959829f2217265
JPsquared <jpjpost1@gmail.com>
2021-04-30 09:23:49 -0400

    Describe the process in greater detail

--- action_planning.py
+++ action_planning.py
@@ -1,7 +1,7 @@
 # MAIN PROJECT EXECUTABLE
 import json
-import dijkstras_algorithm
 import dot_parser
+import a_star_github
 
 
 if __name__ == "__main__":
@@ -12,16 +12,31 @@ if __name__ == "__main__":
 
     # get balloon starting positions
     with open("test_warehouse_start.json") as json_file:
-        start_pos = json.load(json_file)
-        print start_pos
+        start_pos_dict = json.load(json_file)
+        print start_pos_dict
+
     # get balloon ending positions
     with open("test_warehouse_end.json") as json_file:
-        end_pos = json.load(json_file)
-        print end_pos
+        end_pos_dict = json.load(json_file)
+        print end_pos_dict
+
     # generate state graph
-    # edges need to have the f cost and the decision that resulted in their creation
+    # edges need to have the f cost (g cost + h cost) and the decision that resulted in their creation
+    # when calling generate children on a state, child states must be possible, i.e. don't create a state where all
+    # three balloons are being held at once
 
     # run A* on state graph to get optimal decision path
+    # store decision path in a list
 
     # execute list of decisions
-
+    # for each edge in decision path
+        # if decision is drive
+            # get current pos and goal pos
+            # run dijkstra's on hopper map to find path of points
+            # drive from point to point until goal is reached
+        # if decision is pickup balloon
+            # run color project to find balloon
+            # add balloon to payload
+        # if decision is putdown balloon
+            # print that the balloon was put down
+            # remove balloon from payload
